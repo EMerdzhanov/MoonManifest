5a75ad7b4468bff4f3ab407360de3b9e8c225061
Emil Merdzhanov <122936835+EMerdzhanov@users.noreply.github.com>
2026-05-08 07:22:19 -0400

    chore: fix lint issues, remove auto-generated widget test

--- lib/core/moon/moon_phase_engine.dart
+++ lib/core/moon/moon_phase_engine.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: non_constant_identifier_names
 import 'dart:math';
 
 import 'package:moon_manifest/core/moon/moon_constants.dart';
--- test/core/crypto/export_crypto_test.dart
+++ test/core/crypto/export_crypto_test.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:typed_data';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:moon_manifest/core/crypto/export_crypto.dart';
 
